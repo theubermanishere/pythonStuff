f2001e2c241d767c7d135fd870f2450bfab33098
Vinayak Kaushik <vinayakkaushikk@gmail.com>
2019-03-06 21:14:31 +0530

    Added gameentry class

---
+++ gameEntry.py
@@ -0,0 +1,15 @@
+class GameEntry:
+    """Represents one entry of a list of high scores."""
+
+    def __init__(self, name, score):
+        self._name = name
+        self._score = score
+
+    def get_name(self):
+        return self._name
+
+    def get_score(self):
+        return self._score
+
+    def __str__(self):
+        return '({0}, {1})'.format(self._name, self._score)
